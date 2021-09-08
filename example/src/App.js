80b39b1c65b26385575d0cf716355a63c224f821
bowdens <tomfbowden12@gmail.com>
2021-09-08 20:11:43 +1000

    Added sample command to example

--- example/src/App.js
+++ example/src/App.js
@@ -3,9 +3,15 @@ import React from 'react'
 import { Console } from 'react-terminal'
 import 'react-terminal/dist/index.css'
 
+const programs = {
+	echo: ({argv: [_, ...args]}) => {
+		return args.join(" ")
+	},
+}
+
 const App = () => {
 	return (
-		<Console style={{ height: "400px", width: "500px" }} />
+		<Console style={{ height: "400px", width: "500px" }} programs={programs} />
 	)
 }
 
